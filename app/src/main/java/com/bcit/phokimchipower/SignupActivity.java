0151d8600bce1cf9e532e36e5bf7980ff960e969
maryyu97 <kibaco15@gmail.com>
2021-03-12 17:03:15 -0800

    sign up authentication

--- app/src/main/java/com/bcit/phokimchipower/SignupActivity.java
+++ app/src/main/java/com/bcit/phokimchipower/SignupActivity.java
@@ -1,14 +1,171 @@
 package com.bcit.phokimchipower;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class SignupActivity extends AppCompatActivity {
+
+    private static final String TAG = "SignupActivity";
+
+    private FirebaseAuth mAuth;
+    EditText email;
+    EditText name;
+    EditText password;
+    String user_email;
+    String user_name;
+    String user_password;
+    CheckBox agreement;
+    Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        email = findViewById(R.id.email_signup);
+        name = findViewById(R.id.name_signup);
+        password = findViewById(R.id.password_signup);
+
+        user_email = email.getText().toString();
+        user_name = name.getText().toString();
+        user_password = password.getText().toString();
+
+        agreement = findViewById(R.id.agreement_signup);
+
+        signUpButton = findViewById(R.id.signupButton_signup);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createAccount(user_email, user_password);
+                //and then goes to the main page.
+                //delete sign up buttons and sign in buttons.
+                //replace those buttons with user info. ex) "Hello, <username>"
+            }
+        });
+
+
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            currentUser.reload();
+        }
+    }
+
+    private void createAccount(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+        if (!validateForm()) {
+            return;
+        }
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            System.out.println("success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignupActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(SignupActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(SignupActivity.this, "Successfully Signed in", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(SignupActivity.this, "Something went wrong", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+    }
+
+    private void reload() {
+        mAuth.getCurrentUser().reload().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(SignupActivity.this,
+                            "Reload successful!",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Log.e(TAG, "reload", task.getException());
+                    Toast.makeText(SignupActivity.this,
+                            "Failed to reload user.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = user_email;
+        String name = user_name;
+        String password = user_password;
+        boolean isChecked = agreement.isChecked();
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password) || TextUtils.isEmpty(name)) {
+            Toast.makeText(this, "You must enter email, password and name.", Toast.LENGTH_LONG).show();
+            valid = false;
+        }
+        if(!isChecked) {
+            Toast.makeText(this, "You must agree to the term to proceed", Toast.LENGTH_LONG).show();
+            valid = false;
+        }
+        return valid;
+    }
+
 }
+
