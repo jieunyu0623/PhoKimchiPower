768b5119b7965b49d8d7fb2262944540a730fa99
maryyu97 <kibaco15@gmail.com>
2021-04-06 23:50:50 -0700

    UI update

--- app/src/main/java/com/bcit/phokimchipower/AddGrades.java
+++ app/src/main/java/com/bcit/phokimchipower/AddGrades.java
@@ -1,14 +1,55 @@
 package com.bcit.phokimchipower;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Spinner;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 class AddGrades extends AppCompatActivity {
-    
+
+    private DatabaseReference reference;
+    private FirebaseDatabase database;
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    String current_user;
+
+    Spinner evaluation_type;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_grades);
+
+        evaluation_type = findViewById(R.id.evaluation_type_spinner);
+
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("Users");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                snapshot.getValue(user.getClass());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e(TAG, "onCancelled", error.toException());
+            }
+        });
+
+
+
     }
 }
